dcf16c2fe24a148c06ecca3400c4e380d059a586
Loparev.Stanislav <yastas593@mail.ru>
2019-02-02 12:53:14 +0300

    create

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp 			= require('gulp');
+var browserSync 	= require('browser-sync');
+var sass        	= require('gulp-sass');
+var autoprefixer    = require('gulp-autoprefixer');
+var concatCSS       = require('gulp-concat-css');
+var gutil           = require('gulp-util');
+var ftp             = require('gulp-ftp')
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "src/"
+    });
+
+    gulp.watch("src/sass/*.sass", ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("src/sass/*.sass")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+        	browsersin: ['last 2 versions'],
+        	cascade: false
+        }))
+        .pipe(concatCSS('style.css'))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream())
+});
+
+gulp.task('ftp', function(){
+	return gulp.src('src/**')
+	.pipe(ftp({
+            host: '89.108.85.65',
+            user: 'yasta352',
+            pass: 'eNfH18iG3W',
+            remotePath: 'www/loparev-sv.ru/shop'
+        }))
+	.pipe(gutil.noop());
+})
+
+gulp.task('default', ['serve']);
---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+ $(document).ready(function(){
+      $('.main-slider').slick({
+      		infinite: true,
+      		dots: true,
+      		autoplay: true,
+      		speed: 2000,
+      		slidesToShow: 1,
+      		arrows: true
+      });
+    });
